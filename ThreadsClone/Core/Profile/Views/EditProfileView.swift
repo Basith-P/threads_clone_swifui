5f04099f77fa64c6ceaee659be78b6213f430878
Basith P <basithpofficial@gmail.com>
2024-08-27 14:15:43 +0530

    create edit profile view

---
+++ ThreadsClone/Core/Profile/Views/EditProfileView.swift
@@ -0,0 +1,91 @@
+//
+//  EditProfileView.swift
+//  ThreadsClone
+//
+//  Created by Basith on 27/08/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    HStack {
+                        VStack (alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            Text("Kakashi")
+                        }
+                        
+                        Spacer()
+                        
+                        CircularImageView()
+                    }
+                    
+                    Divider()
+                    
+                    VStack (alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        TextField("Enter your bio", text: $bio)
+                    }
+                    
+                    Divider()
+                    
+                    VStack (alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        TextField("Add link", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    Toggle("Private Profile", isOn: $isPrivateProfile)
+                        .fontWeight(.semibold)
+                    
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(.systemGray4))
+                }
+                .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
